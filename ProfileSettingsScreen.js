7ecbc4bcfec02350e8cd6388d1eaeb9732d225df
jesse-barbosa <barbosajesse419@gmail.com>
2024-08-23 23:16:27 -0300

    Funcionalidade para trocar de ícone de perfil, ainda não dinâmico

--- ProfileSettingsScreen.js
+++ ProfileSettingsScreen.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import axios from 'axios';
 import { API_URL } from './apiConfig';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const SettingsScreen = ({ route }) => {
+const ProfileSettingsScreen = ({ route }) => {
   const navigation = useNavigation();
   const { username } = route.params || {};
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedIconIndex, setSelectedIconIndex] = useState(0);
+
+  const icons = ['default', 'man', 'woman'];
 
   useEffect(() => {
-    // Fetch user data
     axios.post(`${API_URL}`, {
       action: 'getUserData',
       username
@@ -20,7 +24,8 @@ const SettingsScreen = ({ route }) => {
       const { success, message, user } = response.data;
       if (success) {
         setUserData(user);
-        fetchUserGoals(user.codUser);
+        const initialIconIndex = icons.indexOf(user.iconUser);
+        setSelectedIconIndex(initialIconIndex >= 0 ? initialIconIndex : 0);
       } else {
         setError(message);
       }
@@ -38,57 +43,98 @@ const SettingsScreen = ({ route }) => {
     default: require('./assets/img/icons/profile/default.png'),
     man: require('./assets/img/icons/profile/man.png'),
     woman: require('./assets/img/icons/profile/woman.png'),
-
   };
-  // Conditionally set image source
-  const imageSource = userData ? images[userData.iconUser] || images['default'] : images['default'];
+
+  const handleNextIcon = () => {
+    setSelectedIconIndex((prevIndex) => (prevIndex + 1) % icons.length);
+  };
+
+  const handlePreviousIcon = () => {
+    setSelectedIconIndex((prevIndex) => (prevIndex - 1 + icons.length) % icons.length);
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando dados...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>Ocorreu um erro ao buscar os dados:</Text>
+        <Text>{error}</Text>
+      </View>
+    );
+  }
+
+  const currentIcon = icons[selectedIconIndex];
+  const imageSource = images[currentIcon] || images['default'];
 
   return (
-  <View style={styles.container}>
-    <ScrollView>
-      <View style={styles.options}>
+    <View style={styles.container}>
+      <View style={styles.selectIcon}>
+        <TouchableOpacity onPress={handlePreviousIcon} style={styles.arrowButton}>
+          <Icon name="chevron-back-outline" style={styles.arrow} />
+        </TouchableOpacity>
+
+        <View style={styles.userImageContainer}>
+          <Image style={styles.userImage} source={imageSource} />
+        </View>
+
+        <TouchableOpacity onPress={handleNextIcon} style={styles.arrowButton}>
+          <Icon name="chevron-forward-outline" style={styles.arrow} />
+        </TouchableOpacity>
       </View>
-    </ScrollView>
-         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.buttonText}>Sair</Text>
-         </TouchableOpacity>
-   </View>
+
+      <View style={styles.details}>
+        <Text style={styles.nameUser}>{userData.nameUser}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.buttonText}>Sair</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    paddingTop: 60,
+    paddingTop: 10,
     padding: 20,
     flex: 1,
     backgroundColor: '#fff',
   },
-  title: {
-    marginTop: 20,
-    color: '#2F1155',
-    fontWeight: 'bold',
-    fontSize: 26,
-    lineHeight: 32,
+  selectIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userImage: {
+    width: 200,
+    height: 200,
+    marginHorizontal: 'auto',
+  },
+  nameUser: {
     textAlign: 'center',
+    fontSize: 28,
+    marginTop: 26,
+    fontWeight: '700',
+    letterSpacing: 1.2,
+    color: '#666666',
   },
-  dataText: {
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  icon: {
-    fontSize: 24,
-    marginRight: 12,
-  },
-  label: {
-    marginLeft: 20,
-    fontWeight: '500',
-    fontSize: 16,
-    color: '#2F1155',
+  arrowButton: {
+    padding: 10,
   },
   arrow: {
-    marginLeft: 'auto',
-    fontSize: 16,
+    fontSize: 40,
     color: '#000',
   },
   button: {
@@ -106,4 +152,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SettingsScreen;
+export default ProfileSettingsScreen;
