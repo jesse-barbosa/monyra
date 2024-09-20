f2b5958893a9497aaa752c126abf836bc98e7e57
jesse-barbosa <barbosajesse419@gmail.com>
2024-09-20 10:30:07 -0300

    Simplificando o sistema

--- banco/dbmonyra.sql
+++ banco/dbmonyra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 04/09/2024 às 16:29
+-- Tempo de geração: 20/09/2024 às 13:55
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -29,20 +29,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tbgoals` (
   `codGoal` int(11) NOT NULL,
-  `nameGoal` char(30) NOT NULL,
-  `categoryGoal` char(25) NOT NULL,
-  `descGoal` char(75) NOT NULL,
+  `nameGoal` varchar(30) DEFAULT NULL,
+  `categoryGoal` varchar(25) DEFAULT NULL,
+  `descGoal` varchar(75) DEFAULT NULL,
   `amountSaved` double NOT NULL DEFAULT 0,
   `amountRemaining` double NOT NULL DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `tbgoals`
---
-
-INSERT INTO `tbgoals` (`codGoal`, `nameGoal`, `categoryGoal`, `descGoal`, `amountSaved`, `amountRemaining`, `created_at`) VALUES
-(71, 'Reserva de emergÃªncia', 'Economia ou Investimentos', '(Essa meta foi gerada automaticamente na criaÃ§Ã£o de sua conta)', 0, 18000, '2024-09-04 14:19:15');
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `userCod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -53,12 +47,12 @@ INSERT INTO `tbgoals` (`codGoal`, `nameGoal`, `categoryGoal`, `descGoal`, `amoun
 CREATE TABLE `tbtransactions` (
   `codTransaction` int(11) NOT NULL,
   `valueTransaction` float NOT NULL,
-  `descTransaction` char(30) NOT NULL,
-  `typeTransaction` char(10) NOT NULL,
-  `categoryTransaction` char(25) NOT NULL,
+  `descTransaction` varchar(30) DEFAULT NULL,
+  `typeTransaction` varchar(10) DEFAULT NULL,
+  `categoryTransaction` varchar(25) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `userCod` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -71,37 +65,11 @@ CREATE TABLE `tbusers` (
   `nameUser` varchar(100) DEFAULT NULL,
   `emailUser` varchar(100) DEFAULT NULL,
   `passwordUser` char(60) NOT NULL,
-  `incomeUser` char(10) NOT NULL,
+  `incomeUser` varchar(10) DEFAULT NULL,
   `balanceUser` float DEFAULT 0,
-  `iconUser` char(50) NOT NULL DEFAULT 'default',
+  `iconUser` varchar(50) DEFAULT 'default',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `tbusers`
---
-
-INSERT INTO `tbusers` (`codUser`, `nameUser`, `emailUser`, `passwordUser`, `incomeUser`, `balanceUser`, `iconUser`, `created_at`) VALUES
-(119, 'JessÃ© Barbosa', 'barbosajesse419@gmail.com', '$2y$10$7w0cFtTW5vzZdtFGoI4gReXBDKazlErdhV.vWiLCLljzK4K/LLdlO', '6000', 0, 'default', '2024-09-04 14:19:15');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `user_goals`
---
-
-CREATE TABLE `user_goals` (
-  `id` int(11) NOT NULL,
-  `userCod` int(11) NOT NULL,
-  `goalCod` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `user_goals`
---
-
-INSERT INTO `user_goals` (`id`, `userCod`, `goalCod`) VALUES
-(28, 119, 71);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Índices para tabelas despejadas
@@ -111,7 +79,9 @@ INSERT INTO `user_goals` (`id`, `userCod`, `goalCod`) VALUES
 -- Índices de tabela `tbgoals`
 --
 ALTER TABLE `tbgoals`
-  ADD PRIMARY KEY (`codGoal`);
+  ADD PRIMARY KEY (`codGoal`),
+  ADD KEY `fk_user_goal` (`userCod`),
+  ADD KEY `fk_user_goal_idx` (`userCod`);
 
 --
 -- Índices de tabela `tbtransactions`
@@ -127,14 +97,6 @@ ALTER TABLE `tbusers`
   ADD PRIMARY KEY (`codUser`);
 
 --
--- Índices de tabela `user_goals`
---
-ALTER TABLE `user_goals`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `userCod` (`userCod`),
-  ADD KEY `goalCod` (`goalCod`);
-
---
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
@@ -142,42 +104,35 @@ ALTER TABLE `user_goals`
 -- AUTO_INCREMENT de tabela `tbgoals`
 --
 ALTER TABLE `tbgoals`
-  MODIFY `codGoal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `codGoal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT de tabela `tbtransactions`
 --
 ALTER TABLE `tbtransactions`
-  MODIFY `codTransaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `codTransaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT de tabela `tbusers`
 --
 ALTER TABLE `tbusers`
-  MODIFY `codUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
-
---
--- AUTO_INCREMENT de tabela `user_goals`
---
-ALTER TABLE `user_goals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `codUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
+-- Restrições para tabelas `tbgoals`
+--
+ALTER TABLE `tbgoals`
+  ADD CONSTRAINT `fk_user_goal_foreign` FOREIGN KEY (`userCod`) REFERENCES `tbusers` (`codUser`);
+
+--
 -- Restrições para tabelas `tbtransactions`
 --
 ALTER TABLE `tbtransactions`
   ADD CONSTRAINT `fk_user_transaction` FOREIGN KEY (`userCod`) REFERENCES `tbusers` (`codUser`);
-
---
--- Restrições para tabelas `user_goals`
---
-ALTER TABLE `user_goals`
-  ADD CONSTRAINT `user_goals_ibfk_1` FOREIGN KEY (`userCod`) REFERENCES `tbusers` (`codUser`),
-  ADD CONSTRAINT `user_goals_ibfk_2` FOREIGN KEY (`goalCod`) REFERENCES `tbgoals` (`codGoal`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
